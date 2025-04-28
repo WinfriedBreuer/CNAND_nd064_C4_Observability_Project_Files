593f8ba048e543a5e9e649a6d8bbe594f275a554
Winfried Breuer <mail@winfried-breuer.de>
2025-04-28 22:02:46 +0200

    added tracing to backend app

--- Project_Starter_Files-Building_a_Metrics_Dashboard/app/backend/app.py
+++ Project_Starter_Files-Building_a_Metrics_Dashboard/app/backend/app.py
@@ -1,12 +1,43 @@
-from flask import Flask, request, jsonify
+import atexit
+
+from flask import Flask, jsonify, request
+from flask_opentracing import FlaskTracing
+from flask_pymongo import PyMongo
+from jaeger_client import Config
+from opentelemetry.instrumentation.flask import FlaskInstrumentor
 from prometheus_flask_exporter import PrometheusMetrics
 
-import pymongo
-from flask_pymongo import PyMongo
-
 app = Flask(__name__)
-metrics = PrometheusMetrics(app)
+FlaskInstrumentor().instrument_app(app)
+
+metrics = PrometheusMetrics(app, group_by='endpoint')
 metrics.info("app_info", "Grafana Prometheus Backend", version="1.0.0")
+
+def init_tracer(service):
+
+    config = Config(
+        config={
+            "sampler": {"type": "const", "param": 1},
+            "logging": True,
+            "reporter_batch_size": 1,
+            "local_agent": {
+                "reporting_host": "localhost",
+                "reporting_port": 6831
+            },            
+        },
+        service_name=service,
+        validate=True
+    )
+
+    # this call also sets opentracing.tracer
+    return config.initialize_tracer()
+
+
+tracer = init_tracer("backend-app")
+flask_tracer = FlaskTracing(tracer, True, app)
+
+atexit.register(lambda: tracer.close())
+
 
 app.config["MONGO_DBNAME"] = "example-mongodb"
 app.config[
